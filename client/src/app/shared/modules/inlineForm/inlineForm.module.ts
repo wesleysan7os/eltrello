4cbb93a97a907f2bc9952ca611100f20706c1aed
wesleysan7os <wesley.santos.comp@gmail.com>
2023-12-25 17:38:21 -0300

    feat: inline form

---
+++ client/src/app/shared/modules/inlineForm/inlineForm.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core"
+import { InlineFormComponent } from "./components/inlineForm.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [InlineFormComponent]
+})
+export class InlineFormModule {}
